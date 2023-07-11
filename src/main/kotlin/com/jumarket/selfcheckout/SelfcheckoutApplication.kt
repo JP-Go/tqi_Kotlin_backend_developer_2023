702e17d2e4cbd374a84716eaecbad329254bd755
João Pedro <jpedro1580@gmail.com>
2023-07-11 08:41:25 -0300

    Move the SpringBoot project to the root of the repo

---
+++ src/main/kotlin/com/jumarket/selfcheckout/SelfcheckoutApplication.kt
@@ -0,0 +1,11 @@
+package com.jumarket.selfcheckout
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfcheckoutApplication
+
+fun main(args: Array<String>) {
+	runApplication<SelfcheckoutApplication>(*args)
+}
---
+++ src/test/kotlin/com/jumarket/selfcheckout/SelfcheckoutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jumarket.selfcheckout
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfcheckoutApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
