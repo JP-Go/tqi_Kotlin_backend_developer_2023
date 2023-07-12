4ddf634a3a2607a902a8f305e3758de722af9a12
João Pedro <jpedro1580@gmail.com>
2023-07-12 14:17:47 -0300

    Implement controller for the product resource
    Implement the controller layer for the product resource

---
+++ src/main/kotlin/com/jumarket/selfcheckout/controllers/ProductController.kt
@@ -0,0 +1,40 @@
+package com.jumarket.selfcheckout.controllers
+
+import com.jumarket.selfcheckout.dtos.ProductDTO
+import com.jumarket.selfcheckout.entities.Product
+import com.jumarket.selfcheckout.services.IProductCategoryService
+import com.jumarket.selfcheckout.services.IProductService
+import com.jumarket.selfcheckout.views.ProductView
+import com.jumarket.selfcheckout.views.productViewfromEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/product")
+class ProductController(
+        val productService: IProductService,
+        val productCategoryService: IProductCategoryService
+) {
+
+    @GetMapping
+    fun getAllProducts(): List<ProductView> {
+        val products = productService.findAll().map { productViewfromEntity(it) }
+        return products
+    }
+
+    @GetMapping("{productId}")
+    fun getProduct(@PathVariable productId: Long): ProductView {
+        val product = productService.findById(productId)
+        return productViewfromEntity(product)
+    }
+
+    @PostMapping
+    fun createProduct(@RequestBody dto: ProductDTO): Product {
+        val savedProduct = productService.createProduct(dto)
+        return savedProduct
+    }
+}
