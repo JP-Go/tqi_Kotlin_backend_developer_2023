d19e982638850cf36a3ac4734aa87f5d640c5775
João Pedro <jpedro1580@gmail.com>
2023-07-18 21:28:43 -0300

    Add validation annotations on Controllers
    Add the @Valid annotation on parameters of controllers methods and

--- src/main/kotlin/com/jumarket/selfcheckout/dtos/ProductDTO.kt
+++ src/main/kotlin/com/jumarket/selfcheckout/dtos/ProductDTO.kt
@@ -6,14 +6,14 @@ import jakarta.validation.constraints.PositiveOrZero
 import java.math.BigDecimal
 
 data class ProductDTO(
-                @field:NotNull(message = "Missing name")
-                @field:NotBlank(message = "Name must have at least 1 character")
-                val name: String,
-                @field:NotNull(message = "Missing unit")
-                @field:NotBlank(message = "Unit must have at least 1 character")
-                val unit: String,
-                @field:NotNull(message = "Missing price")
-                @field:PositiveOrZero(message = "Price must be a positive number")
-                val price: BigDecimal,
-                var productCategory: Long?
+        @field:NotNull(message = "Missing name")
+        @field:NotBlank(message = "Name must have at least 1 character")
+        val name: String,
+        @field:NotNull(message = "Missing unit")
+        @field:NotBlank(message = "Unit must have at least 1 character")
+        val unit: String,
+        @field:NotNull(message = "Missing price")
+        @field:PositiveOrZero(message = "Price must be a positive number")
+        val price: BigDecimal,
+        var productCategory: Long?
 )
--- src/main/kotlin/com/jumarket/selfcheckout/dtos/RemoveCartItemDTO.kt
+++ src/main/kotlin/com/jumarket/selfcheckout/dtos/RemoveCartItemDTO.kt
@@ -1,5 +1,10 @@
 package com.jumarket.selfcheckout.dtos
 
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 
-data class RemoveCartItemDTO(@field:NotNull(message = "Missing productId") var productId: Long)
+data class RemoveCartItemDTO(
+        @field:NotNull(message = "Missing productId")
+        @field:Positive(message = "productId must be positive number")
+        var productId: Long
+)
