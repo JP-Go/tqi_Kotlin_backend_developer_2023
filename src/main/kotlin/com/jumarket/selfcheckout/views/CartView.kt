2dbeb3fd48fc8f9991171975ebcbb3e5c1fbceee
João Pedro <jpedro1580@gmail.com>
2023-07-27 22:54:59 -0300

    Add to cart view
    Adds total of the cart items and payment information to the cart view

--- src/main/kotlin/com/jumarket/selfcheckout/views/CartView.kt
+++ src/main/kotlin/com/jumarket/selfcheckout/views/CartView.kt
@@ -1,8 +1,26 @@
 package com.jumarket.selfcheckout.views
 
 import com.jumarket.selfcheckout.entities.Cart
+import com.jumarket.selfcheckout.entities.CartItem
+import com.jumarket.selfcheckout.enumerations.PaymentMethod
+import java.math.BigDecimal
 
-data class CartView(val id: Long, val items: List<CartItemView>)
+data class CartView(
+        val id: Long,
+        val items: List<CartItemView>,
+        val paid: Boolean,
+        val total: BigDecimal,
+        val method: PaymentMethod?
+)
 
 fun cartFromEntity(entity: Cart) =
-        CartView(id = entity.id as Long, items = entity.items.map { cartItemViewFromEntity(it) })
+        CartView(
+                id = entity.id as Long,
+                items = entity.items.map { cartItemViewFromEntity(it) },
+                paid = entity.paid,
+                method = entity.paymentMethod,
+                total =
+                        entity.items.fold(BigDecimal.ZERO) { a: BigDecimal, b: CartItem ->
+                            a + b.totalPrice
+                        }
+        )
